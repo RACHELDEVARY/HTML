bbc8e5138b72e201d271067dfed791502103804a
RACHELDEVARY <55826044+RACHELDEVARY@users.noreply.github.com>
2021-12-19 11:13:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-// var p         = document.querySelector('p');
-// var index     = 0;
-// var sentences = [
-//   "Ticking away the moments that make up a dull day",
-//   "You fritter and waste the hours in an offhand way. ",
-//   "Kicking around on a piece of ground in your home town ",
-//   "Waiting for someone or something to show you the way. "
-// ];
-
-// function toggleText() {
-//   p.innerHTML = sentences[index];
-//   index = (index + 1) % sentences.length;
-// }
-
-// window.addEventListener('click', toggleText);
-let page1 = "block";
-let page2 = "none";
-
-// function toggleText() {
-//   p.innerHTML = sentences[index];
-//   index = (index + 1) % sentences.length;
-// }
 
 
 function myFunction() {
